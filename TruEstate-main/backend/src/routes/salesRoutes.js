c4bedd499f48b2d94f885312b1dcfcf672158688
dussaanushka1605 <dussaanushka@gmail.com>
2025-12-09 12:25:31 +0530

    Update project working version

--- TruEstate-main/backend/src/routes/salesRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getSalesData } from "../controllers/salesController.js";
-
-const router = express.Router();
-
-router.get("/", getSalesData);
-
-export default router;
